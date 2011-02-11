170e09c75f28a7f90130d7825cb902a58c9d92c3
ljones <ljones@git.lsstcorp.org>
2011-02-11 02:29:02 +0000

    updated calib doc .. just before editing Internal Details

--- calib_figs_mags2.py
+++ calib_figs_mags2.py
@@ -16,7 +16,7 @@ def read_hardware(shift_perc=None):
     # read system (hardware) transmission)
     filterdir = os.getenv("LSST_THROUGHPUTS_DEFAULT")
     hardware = ("detector.dat", "m1.dat", "m2.dat", "m3.dat", "lens1.dat", "lens2.dat", "lens3.dat")
-    # Read in the standard components, except shift the filter by 1% of the central wavelength near the edge
+    # Read in the standard components, but potentially shift the filter by shift_perc percent.
     sys = {}
     for f in filterlist:
         sys[f] = Bandpass()
@@ -53,31 +53,20 @@ def read_atmos():
 def combine_throughputs(atmos, sys):
     # Set up the total throughput for this system bandpass, using the variety of atmospheres. 
     total = {}
-    key = 'standard'
-    total[key] = {}
-    for f in filterlist:
-        wavelen, sb = sys[f].multiplyThroughputs(atmos['Standard'].wavelen, atmos['Standard'].sb)
-        total[key][f] = Bandpass(wavelen, sb)
-        total[key][f].sbTophi()
-    key = 'X=1.0'
-    total[key] = {}
-    for f in filterlist:
-        wavelen, sb = sys[f].multiplyThroughputs(atmos['X=1.0'].wavelen, atmos['X=1.0'].sb)
-        total[key][f] = Bandpass(wavelen, sb)
-        total[key][f].sbTophi()
-    key = 'X=1.5'
-    total[key] = {}
-    for f in filterlist:
-        wavelen, sb = sys[f].multiplyThroughputs(atmos['X=1.5'].wavelen, atmos['X=1.5'].sb)
-        total[key][f] = Bandpass(wavelen, sb)
-        total[key][f].sbTophi()
+    for key in atmos.keys():
+        total[key] = {}
+        for f in filterlist:
+            wavelen, sb = sys[f].multiplyThroughputs(atmos[key].wavelen, atmos[key].sb)
+            total[key][f] = Bandpass(wavelen, sb)
+            total[key][f].sbTophi()
     return total
 
 
 def read_stars():
     # read MS, g40 stars SEDs
     stars = {}
-    stardir = "/Users/rhiannonjones/seds/kurucz_r/"
+    homedir = os.getenv("HOME")    
+    stardir = os.path.join(homedir, "seds/kurucz_r")
     allfilelist = os.listdir(stardir)
     starlist = []
     for filename in allfilelist:
@@ -85,8 +74,8 @@ def read_stars():
             starlist.append(filename)
     stars = {}
     for s in starlist:
-        stars[s] = Sed.Sed()
-        stars[s].readSED_flambda(stardir+s)
+        stars[s] = Sed()
+        stars[s].readSED_flambda(os.path.join(stardir,s))
     print "#Read %d stars from %s" %(len(starlist), stardir)
     atemperature = []
     amet = []
@@ -99,52 +88,58 @@ def read_stars():
         amet.append(met)
         temperature = float(tmp[1][:5])
         atemperature.append(temperature)
-    temperature = n.array(atemperature)
-    met = n.array(amet)
-    return stars, starlist, temperature, met
+    temperature = numpy.array(atemperature)
+    met = numpy.array(amet)
+    i = 0
+    starlist2 = []
+    for s in starlist:
+        if (temperature[i] > 5000.0):
+            starlist2.append(s)
+        i =  i + 1
+    return stars, starlist2, temperature, met
 
 
 if __name__ == "__main__":
     
     sys_std = read_hardware(shift_perc=None)
+    sys_shift = read_hardware(shift_perc=0.01)
+    atmokeylist = ['standard', 'X=1.0', 'X=1.5']
     atmos = read_atmos()
     total_std = combine_throughputs(atmos, sys_std)    
-    stars, starlist = read_stars()
-    # Use a bandpass to set up the fnuArray for easy/fast calculation
-    # of magnitudes for the stars.
-    starfnuArray, starzp = total['standard']['g'].setupFnuArray(stars)
-    # calculate the standard magnitudes for each of these stars
+    total_shift = combine_throughputs(atmos, sys_shift)
+    stars, starlist, temperatures, metallicity = read_stars()
+    print temperatures.max(), temperatures.min(), metallicity.max(), metallicity.min()
+    print len(starlist)
+    # calculate the standard and shifted magnitudes for each of these stars
     mags_std = {}
-    atmokeylist = ['standard', 'X=1.0', 'X=1.5']
-    for akey in atmokeylist:
-        bplist = []
-        for f in filterlist:
-            bplist.append(total[akey][f])
-
-    
-    shifts = numpy.arange(0, 10, 0.5)
-    for shift in shifts:
-        sys = read_hardware(shift_perc = shift)
-        total = combine_throughputs(atmos, sys)
-        
-        # calculate magnitude in flat bandpass
+    mags_shift = {}
+    for f in filterlist:
+        mags_std[f] = numpy.zeros(len(starlist), dtype='float')
+        mags_shift[f] = numpy.zeros(len(starlist), dtype='float')
+        i = 0
         for s in starlist:
-            stdmags = {}
-        writestring = "%s  standard " %(s)
-        for f in filterlist:
-            stdmags[f] = stars[s].calcMag(standard[f])
-            writestring = writestring + " %.5f " %(stdmags[f])
-        for f in filterlist:
-            writestring = writestring + " 0.0 "
-        print writestring
-        for a in atmoslist:
-            writestring = "%s  %s " %(s, a)
-            mags = {}
-            for f in filterlist:
-                mags[f] = stars[s].calcMag(throughputs[a][f])
-                writestring = writestring + " %.5f " %(mags[f])
-            for f in filterlist:
-                tmp = mags[f] - stdmags[f]
-                writestring = writestring + " %.5f " %(tmp)
-            print writestring
+            mags_std[f][i] = stars[s].calcMag(total_std['Standard'][f])
+            mags_shift[f][i] = stars[s].calcMag(total_shift['X=1.5'][f])
+            i = i + 1
+    gi = mags_std['g'] - mags_std['i']
+    shifts = {}
+    for f in filterlist:
+        shifts[f] = mags_shift[f] - mags_std[f]
 
+    pylab.figure()
+    pylab.subplots_adjust(top=0.93, wspace=0.3, hspace=0.32, bottom=0.09, left=0.12, right=0.96)
+    i = 1
+    for f in filterlist:        
+        pylab.subplot(3,2,i)
+        pylab.plot(gi, shifts[f], 'k.')
+        pylab.xlabel("g-i")
+        pylab.ylabel("Delta %s " %(f))
+        if f == 'u':
+            pylab.ylim(-0.11, 0.03)
+        else:
+            pylab.ylim(-0.04, 0.025)
+        pylab.grid(True)
+        i = i + 1
+    pylab.figtext(0.2, 0.95, "Change in magnitude for X=1.5 and Filter shift of 1%")
+    #pylab.show()
+    pylab.savefig("delta_mags2.eps", format='eps')
